26b5707475241936a9beace120fa7f235715465d
Anne Chretien <chretien.an@gmail.com>
2022-08-29 11:10:16 +0200

    tailwind add

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,20 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <nav>
-      <Link href="/">
-        <a>MarsKetplace</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/futures">
-        <a>Futures</a>
-      </Link>
-      <ConnectButton />
+    <nav className="items-center">
+      <h1 className="py-4 px-4 font-bold text-3xl">NFT MarsKetplace</h1>
+      <div>
+        <Link href="/">
+          <a>MarsKetplace</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/futures">
+          <a>Futures</a>
+        </Link>
+        <ConnectButton />
+      </div>
     </nav>
   )
 }
