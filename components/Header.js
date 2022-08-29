d1457deb681430b2feee6c4d7f7de5d6304c10c5
Anne Chretien <chretien.an@gmail.com>
2022-08-29 10:51:33 +0200

    added futures page

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,19 @@
-import { ConnectButton } from 'web3uikit'
+import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
 export default function Header() {
-  return <ConnectButton />
+  return (
+    <nav>
+      <Link href="/">
+        <a>MarsKetplace</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/futures">
+        <a>Futures</a>
+      </Link>
+      <ConnectButton />
+    </nav>
+  )
 }
