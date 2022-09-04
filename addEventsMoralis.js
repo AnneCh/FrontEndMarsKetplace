9c21df18ecf4db9fbe0d01b845a4c614e389dd9a
Anne Chretien <chretien.an@gmail.com>
2022-09-04 17:06:04 +0200

    addEvents fail

--- addEventsMoralis.js
+++ addEventsMoralis.js
@@ -3,3 +3,146 @@
 
 const Moralis = require("moralis/node")
 require("dotenv").config()
+
+const contractAddresses = require("./constants/networkMappings.json")
+let chainId = process.env.chainId || 31337
+//moralis understands that localchainid is 1337, so there's a need to convert it
+let moralisChainId = chainId == "31337?" ? "1337" : chainId
+
+const contractAddress = contractAddresses[chainId]["MarsKetplace"][0]
+const serverUrl = process.env.NEXT_PUBLIC_SERVERURL
+const appId = process.env.NEXT_PUBLIC_APPID
+const masterKey = process.env.MORALIS_API_KEY
+
+async function main() {
+  await Moralis.start({ serverUrl, appId, masterKey })
+  console.log(`Working with ${contractAddress}`)
+
+  let NFTListedOptions = {
+    chainId: moralisChainId,
+    sync_historical: true,
+    topic: "NFTListed(address,address,uint256,uint256)",
+    abi: {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "seller",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "nftAddress",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+      ],
+      name: "NFTListed",
+      type: "event",
+    },
+    tableName: "NFT Listed",
+    address: contractAddress,
+  }
+
+  let NFTBoughtOptions = {
+    chainId: moralisChainId,
+    sync_historical: true,
+    topic: "NFTBought(address,address,uint256,uint256)",
+    abi: {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "buyer",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "nftAddress",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+      ],
+      name: "NFTBought",
+      type: "event",
+    },
+    tableName: "NFT Bought",
+    address: contractAddress,
+  }
+
+  let NFTDeletedOptions = {
+    chainId: moralisChainId,
+    sync_historical: true,
+    topic: "NFTDeleted(address,uint256)",
+    abi: {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "nftAddress",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "uint256",
+          name: "tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "NFTDeleted",
+      type: "event",
+    },
+    tableName: "NFT Deleted",
+    address: contractAddress,
+  }
+
+  const ListedResponse = await Moralis.Cloud.run("watchContractEvent", NFTListedOptions, {
+    useMasterKey: true,
+  })
+  const BoughtResponse = await Moralis.Cloud.run("watchContractEvent", NFTBoughtOptions, {
+    useMasterKey: true,
+  })
+  const DeletedResponse = await Moralis.Cloud.run("watchContractEvent", NFTDeletedOptions, {
+    useMasterKey: true,
+  })
+
+  if (ListedResponse.success && BoughtResponse.success && DeletedResponse.success) {
+    console.log("Database successfully updated with watching events!")
+  } else {
+    console.log("Something went wrong...")
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
