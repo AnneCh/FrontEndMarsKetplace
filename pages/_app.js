86a5c65b1ed46bd6da698e9566bbfd7a07f0b417
Anne Chretien <chretien.an@gmail.com>
2022-09-04 10:17:13 +0200

    starting server

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,12 @@
 import "../styles/globals.css"
-import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
-import { NotificationProvider } from "web3uikit"
-
-const APPID = process.env.NEXT_PUBLIC_APPID
-const SERVER = process.env.NEXT_PUBLIC_SERVERURL
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider appId={APPID} serverUrl={SERVER}>
-      <NotificationProvider>
-        <Header />
-        <Component {...pageProps} />
-      </NotificationProvider>
-    </MoralisProvider>
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
-import Image from "next/image"
+import Moralis from "moralis"
 import styles from "../styles/Home.module.css"
 
-export default function Home() {
+function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +12,30 @@ export default function Home() {
       </Head>
       <h1 className="font-bold text-3xl">Own your plot on MARS</h1>
       <h3>Here are the current NFTs for sale</h3>
+      <div>
+        <p>Display NFT here</p>
+      </div>
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  // reads the api key from .env.local and starts Moralis SDK
+  await Moralis.start({ apiKey: process.env.NEXT_MORALIS_API_KEY })
+
+  //const address = "0x..."
+
+  //const nativeBalance = await Moralis.EvmApi.account.getNativeBalance({
+  //  address,
+  //})
+
+  return {
+    props: {
+      // address,
+      // Return the native balance formatted in ether via the .ether getter
+      //nativeBalance: nativeBalance.result.balance.ether,
+    },
+  }
+}
+
+export default Home
