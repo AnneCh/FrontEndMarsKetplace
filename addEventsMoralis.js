136c46a6a5405f57e2db159c01fb8a71cd93a960
Anne Chretien <chretien.an@gmail.com>
2022-09-04 12:24:30 +0200

    scripts events listeners

---
+++ addEventsMoralis.js
@@ -0,0 +1,5 @@
+//script that will ask moralis server to listen to specific events
+// need to create a specific script in our back-end that will trigger this script
+
+const Moralis = require("moralis/node")
+require("dotenv").config()
