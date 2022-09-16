06b8aa3a7b01af6bc6f6b09cfe38ce6b3c318304
Anne Chretien <chretien.an@gmail.com>
2022-09-16 15:23:47 +0200

    listed nft data displayed

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,42 @@
 import Head from "next/head"
 import Moralis from "moralis"
 import styles from "../styles/Home.module.css"
+import mars from "./mars.jpg"
+import { useMoralisQuery } from "react-moralis"
 
 function Home(props) {
+  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+    //table name
+    "ActiveNFT",
+    // function for query
+    (query) => query.limit(10).descending("tokenId")
+  )
+  console.log(listedNfts)
+
   return (
-    <div className={styles.container}>
+    <div backgroundImage={mars} height="100vp">
       <Head>
         <title>The MarsKetplace</title>
         <meta name="description" content="NFT MarsKetplace" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="font-bold text-3xl">Own your plot on MARS</h1>
-      <h3>Here are the current NFTs for sale</h3>
+      <h3 className="text-2xl">Here are the current NFTs for sale</h3>
       <div>
         <p>Display NFT here</p>
+        {fetchingListedNfts ? (
+          <div>Loading...</div>
+        ) : (
+          listedNfts.map((nft) => {
+            console.log(nft.attributes)
+            const { price, nftAddress, tokenId, marsKetplaceAddress, seller } = nft.attributes
+            return (
+              <div>
+                Price : {price}, NFT address : {nftAddress}, Token ID : {tokenId}, Seller :{seller}
+              </div>
+            )
+          })
+        )}
       </div>
     </div>
   )
@@ -23,18 +46,8 @@ export async function getServerSideProps(context) {
   // reads the api key from .env.local and starts Moralis SDK
   await Moralis.start({ apiKey: process.env.MORALIS_API_KEY })
 
-  //const address = "0x..."
-
-  //const nativeBalance = await Moralis.EvmApi.account.getNativeBalance({
-  //  address,
-  //})
-
   return {
-    props: {
-      // address,
-      // Return the native balance formatted in ether via the .ether getter
-      //nativeBalance: nativeBalance.result.balance.ether,
-    },
+    props: {},
   }
 }
 
