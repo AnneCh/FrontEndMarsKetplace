a4ad8f166163493588dceac4c3c77a4293ae7501
Anne Chretien <chretien.an@gmail.com>
2022-09-22 11:11:58 +0200

    added subtitle

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,12 @@ import Link from "next/link"
 export default function Header() {
   return (
     <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-      <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+      <div>
+        <h1 className="py-4 px-4 font-bold text-4xl">The One and Only MarsKetplace</h1>
+        <p className=" px-4">
+          The only place in the galaxy where you can buy your own plot of land on Mars!
+        </p>
+      </div>
       <div className="flex flex-row items-center space-between">
         <Link href="/">
           <a className="mr-4 p-6">Home</a>
